2705dc63f4ea1c9c81dff9bee9da438f0353b878
Sonali Purwar <>
2021-08-29 12:38:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import ConvertInput from "./components/convertInput";
+import ConvertOutput from "./components/convertOutput";
+import convertToRoman from "./utils/converToRoman";
+import convertToNumeral from "./utils/convertToNumeral";
+import { useState } from 'react';
 
 function App() {
+  const [romanValue, setRomanValue] = useState();
+
+  const handleInput= (e)=> {
+    let result = convertToRoman(e.target.value);
+    setRomanValue({romanValue : result});
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="App container">
+          <h1 className="container__label">Roman Numeral Converter</h1>
+          <ConvertInput onChange={handleInput} />
+          <ConvertOutput value={romanValue} />
+        </div>
     </div>
   );
 }
