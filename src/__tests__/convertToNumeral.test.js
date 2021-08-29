b561f4105c789b5318c1b52d5afeb89b631533ee
Sonali Purwar <>
2021-08-30 02:26:16 +0530

    Updates

--- src/__tests__/convertToNumeral.test.js
+++
@@ -1,8 +0,0 @@
-import convertToNumeral from '../utils/convertToNumeral';
-
-test('it can convert to roman',() => {
-    expect(convertToNumeral('MM')).toBe(2000);
-    expect(convertToNumeral('IV')).toBe(4);
-    expect(convertToNumeral('MMMCMXCIX')).toBe(3999);
-    expect(convertToNumeral('CCXLV')).toBe(245);
-});
